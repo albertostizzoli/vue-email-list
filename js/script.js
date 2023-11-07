950183a1f3202739b8261bc64b84e4212b5dcb4c
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-07 15:35:08 +0100

    Create Array and Function getEmails

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-
+             emails: []
         }
     },
     methods:{
-    }
+        getEmails(){
+            this.emails = [];
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) =>{
+                    this.emails.push(result.data.response);
+                })
+            }
+        }
+    },
 }).mount('#app');
